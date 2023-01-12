df7f2663ad1329fe5042fe50f46afb1ee0cfb8fa
ADTPAspres <84079200+ADTPAspres@users.noreply.github.com>
2023-01-12 11:51:21 +0100

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -29,29 +29,40 @@ var lyr_CPThuir_1 = new ol.layer.Vector({
     <img src="styles/legend/CPThuir_1_1.png" /> HLM<br />\
     <img src="styles/legend/CPThuir_1_2.png" /> Lotissement<br />'
         });
-var format_SiteCP_2 = new ol.format.GeoJSON();
-var features_SiteCP_2 = format_SiteCP_2.readFeatures(json_SiteCP_2, 
+var format_Composteursindividuels_2 = new ol.format.GeoJSON();
+var features_Composteursindividuels_2 = format_Composteursindividuels_2.readFeatures(json_Composteursindividuels_2, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_SiteCP_2 = new ol.source.Vector({
+var jsonSource_Composteursindividuels_2 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_SiteCP_2.addFeatures(features_SiteCP_2);
-var lyr_SiteCP_2 = new ol.layer.Vector({
+jsonSource_Composteursindividuels_2.addFeatures(features_Composteursindividuels_2);
+var lyr_Composteursindividuels_2 = new ol.layer.Vector({
                 declutter: true,
-                source:jsonSource_SiteCP_2, 
-                style: style_SiteCP_2,
+                source:jsonSource_Composteursindividuels_2, 
+                style: style_Composteursindividuels_2,
                 interactive: true,
-                title: '<img src="styles/legend/SiteCP_2.png" /> Site CP'
-            });
+    title: 'Composteurs individuels<br />\
+    <img src="styles/legend/Composteursindividuels_2_0.png" /> 2012<br />\
+    <img src="styles/legend/Composteursindividuels_2_1.png" /> 2013<br />\
+    <img src="styles/legend/Composteursindividuels_2_2.png" /> 2014<br />\
+    <img src="styles/legend/Composteursindividuels_2_3.png" /> 2015<br />\
+    <img src="styles/legend/Composteursindividuels_2_4.png" /> 2016<br />\
+    <img src="styles/legend/Composteursindividuels_2_5.png" /> 2017<br />\
+    <img src="styles/legend/Composteursindividuels_2_6.png" /> 2018<br />\
+    <img src="styles/legend/Composteursindividuels_2_7.png" /> 2019<br />\
+    <img src="styles/legend/Composteursindividuels_2_8.png" /> 2020<br />\
+    <img src="styles/legend/Composteursindividuels_2_9.png" /> 2021<br />\
+    <img src="styles/legend/Composteursindividuels_2_10.png" /> 2022<br />'
+        });
 
-lyr_GoogleHybrid_0.setVisible(true);lyr_CPThuir_1.setVisible(true);lyr_SiteCP_2.setVisible(true);
-var layersList = [lyr_GoogleHybrid_0,lyr_CPThuir_1,lyr_SiteCP_2];
+lyr_GoogleHybrid_0.setVisible(true);lyr_CPThuir_1.setVisible(true);lyr_Composteursindividuels_2.setVisible(true);
+var layersList = [lyr_GoogleHybrid_0,lyr_CPThuir_1,lyr_Composteursindividuels_2];
 lyr_CPThuir_1.set('fieldAliases', {'id': 'id', 'Type': 'Type', 'Nbrhabitat': 'Nbrhabitat', });
-lyr_SiteCP_2.set('fieldAliases', {'id': 'id', });
+lyr_Composteursindividuels_2.set('fieldAliases', {'Nom de l\'a': 'Nom de l\'a', 'Adresse de': 'Adresse de', 'Commune': 'Commune', 'Nombre de': 'Nombre de', 'Observatio': 'Observatio', 'Date d\'acq': 'Date d\'acq', 'Ann�e': 'Ann�e', 'Propri�t': 'Propri�t', 'Coordonn��': 'Coordonn��', 'Mail': 'Mail', 'Nombre d_1': 'Nombre d_1', 'superficie': 'superficie', 'Observat_1': 'Observat_1', 'latitude': 'latitude', 'longitude': 'longitude', 'result_lab': 'result_lab', 'result_sco': 'result_sco', 'result_typ': 'result_typ', 'result_id': 'result_id', 'result_hou': 'result_hou', 'result_nam': 'result_nam', 'result_str': 'result_str', 'result_pos': 'result_pos', 'result_cit': 'result_cit', 'result_con': 'result_con', 'result_c_1': 'result_c_1', 'result_old': 'result_old', 'result_o_1': 'result_o_1', 'result_dis': 'result_dis', });
 lyr_CPThuir_1.set('fieldImages', {'id': 'TextEdit', 'Type': 'TextEdit', 'Nbrhabitat': 'TextEdit', });
-lyr_SiteCP_2.set('fieldImages', {'id': 'TextEdit', });
+lyr_Composteursindividuels_2.set('fieldImages', {'Nom de l\'a': 'TextEdit', 'Adresse de': 'TextEdit', 'Commune': 'TextEdit', 'Nombre de': 'TextEdit', 'Observatio': 'TextEdit', 'Date d\'acq': 'TextEdit', 'Ann�e': 'TextEdit', 'Propri�t': 'TextEdit', 'Coordonn��': 'TextEdit', 'Mail': 'TextEdit', 'Nombre d_1': 'TextEdit', 'superficie': 'TextEdit', 'Observat_1': 'TextEdit', 'latitude': 'TextEdit', 'longitude': 'TextEdit', 'result_lab': 'TextEdit', 'result_sco': 'TextEdit', 'result_typ': 'TextEdit', 'result_id': 'TextEdit', 'result_hou': 'TextEdit', 'result_nam': 'TextEdit', 'result_str': 'TextEdit', 'result_pos': 'TextEdit', 'result_cit': 'TextEdit', 'result_con': 'TextEdit', 'result_c_1': 'TextEdit', 'result_old': 'TextEdit', 'result_o_1': 'TextEdit', 'result_dis': 'TextEdit', });
 lyr_CPThuir_1.set('fieldLabels', {'id': 'no label', 'Type': 'no label', 'Nbrhabitat': 'no label', });
-lyr_SiteCP_2.set('fieldLabels', {'id': 'no label', });
-lyr_SiteCP_2.on('precompose', function(evt) {
+lyr_Composteursindividuels_2.set('fieldLabels', {'Nom de l\'a': 'no label', 'Adresse de': 'no label', 'Commune': 'no label', 'Nombre de': 'no label', 'Observatio': 'no label', 'Date d\'acq': 'no label', 'Ann�e': 'no label', 'Propri�t': 'no label', 'Coordonn��': 'no label', 'Mail': 'no label', 'Nombre d_1': 'no label', 'superficie': 'no label', 'Observat_1': 'no label', 'latitude': 'no label', 'longitude': 'no label', 'result_lab': 'no label', 'result_sco': 'no label', 'result_typ': 'no label', 'result_id': 'no label', 'result_hou': 'no label', 'result_nam': 'no label', 'result_str': 'no label', 'result_pos': 'no label', 'result_cit': 'no label', 'result_con': 'no label', 'result_c_1': 'no label', 'result_old': 'no label', 'result_o_1': 'no label', 'result_dis': 'no label', });
+lyr_Composteursindividuels_2.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
---
+++ styles/Composteursindividuels_2_style.js
@@ -0,0 +1,131 @@
+var size = 0;
+var placement = 'point';
+function categories_Composteursindividuels_2(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                switch(value.toString()) {case '2012':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 6.000000000000002 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(247,251,255,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '2013':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 6.000000000000002 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(228,239,249,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '2014':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 6.000000000000002 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(209,226,243,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '2015':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 6.000000000000002 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(186,214,235,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '2016':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 6.000000000000002 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(154,200,224,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '2017':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 6.000000000000002 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(115,178,216,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '2018':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 6.000000000000002 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(82,157,204,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '2019':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 6.000000000000002 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(53,133,191,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '2020':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 6.000000000000002 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(29,108,177,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '2021':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 6.000000000000002 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(8,81,156,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '2022':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 6.000000000000002 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(8,48,107,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_Composteursindividuels_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("Ann�e");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+var style = categories_Composteursindividuels_2(feature, value, size, resolution, labelText,
+                          labelFont, labelFill, bufferColor,
+                          bufferWidth, placement);
+
+    return style;
+};
